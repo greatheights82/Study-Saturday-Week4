9d1437ba42109c533fd6265438b9522dcb8512a9
Jake Hebert <greatheights@gmail.com>
2019-02-09 12:21:16 -0600

    cycle 2 complete

--- browser/components/NewStudentForm.js
+++ browser/components/NewStudentForm.js
@@ -1,22 +1,65 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class NewStudentForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstName: '',
+      lastName: '',
+      email: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    console.log('before:', this.state[event.target.name]);
+    this.setState({ [event.target.name]: event.target.value });
+    console.log('after: ', this.state[event.target.name]);
+  }
+
+  async handleSubmit(event) {
+    event.preventDefault();
+    this.setState({
+      firstName: '',
+      lastName: '',
+      email: '',
+    });
+  }
+
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
         <label>
           First Name:
-          <input type="text" name="firstName" />
+          <input
+            type="text"
+            name="firstName"
+            onChange={this.handleChange}
+            value={this.state.firstName}
+          />
         </label>
 
         <label>
           Last Name:
-          <input type="text" name="lastName" />
+          <input
+            type="text"
+            name="lastName"
+            onChange={this.handleChange}
+            value={this.state.lastName}
+          />
         </label>
 
         <label>
           Email:
-          <input type="email" name="email" />
+          <input
+            type="email"
+            name="email"
+            onChange={this.handleChange}
+            value={this.state.email}
+          />
         </label>
 
         <button type="submit">Submit New Student</button>
